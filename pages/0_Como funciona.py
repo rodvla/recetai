8e437fedd7afdfcfccdfd9a53e0cd7765c41e721
Rod Vlæminck <rodrigo.vlaeminck@nowports.com>
2024-01-11 00:05:47 +0000

    final version

---
+++ pages/0_Como funciona.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Creador de Dieta Semanal")
+st.header("Ingresas los ingredientes sugeridos en el textarea y haces ctrl+enter")
+st.header("Queda pensando y te devuelve una dieta semanal en texto")
